8d0121f061a3337fdde4afcbc6b72ad032c97aa7
amitbis9e <148409640+amitbis9e@users.noreply.github.com>
2026-09-08 21:30:17 +0530

    main fxn

---
+++ DSA_cpp /01_basics/main.cpp
@@ -0,0 +1,16 @@
+// #include<iostream>
+// using namespace std;
+// int main(){
+// cout<<"first c++ program"<<endl;
+// return 0;
+// }
+
+// Alternative way
+
+#include<iostream>
+
+int main(){
+
+std::cout<<"first c++ program\n";
+
+}
