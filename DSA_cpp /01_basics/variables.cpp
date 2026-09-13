6e3388286fe7aa3b35ea9ed5faf82d76588371ba
amitbis9e <148409640+amitbis9e@users.noreply.github.com>
2026-09-13 07:57:36 +0530

    variables and datatypes

---
+++ DSA_cpp /01_basics/variables.cpp
@@ -0,0 +1,29 @@
+// Variables and Datatypes
+// variable is named stored memory lcoation
+// datatype specifies the type and size of variable stored in memory
+
+#include <iostream>
+using namespace std;
+int main(){
+   //int 
+   int count = 5;
+   // float
+   float share = 3.14;
+   // char
+   char alphabet = 'z';
+   // double
+   double weight = 55.69887;
+   // boolean
+   bool isMale = true;
+   bool isBoy = 1;      // create but not use
+   bool isFemale = false;
+   bool isGirl = 0;
+
+   cout<<count<<endl;
+   cout<<share<<endl;
+   cout<<alphabet<<endl;
+   cout<<weight<<endl;
+   cout<<isMale<<endl;
+   cout<<isGirl<<endl;
+
+}
